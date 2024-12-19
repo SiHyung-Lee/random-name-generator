e1b0ffb5feac9fc21167e0502d9232ec5f55521c
sihyung <l_sh@athomecorp.com>
2024-12-19 19:13:57 +0900

    Add name generation logic and associated data handling
    Integrated a JSON file containing surnames and given names and implemented state management for dynamic name generation. Updated input handling to reflect user-provided generation count and added a placeholder for generated names in the UI. Prepared groundwork for future enhancements to the name generation feature.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import data from "./data.json";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [isGenerated, setIsGenerated] = useState(false);
+  const [name, setName] = useState([]);
+  // const [surnames, setSurnames] = useState([]);
+
+  const handleGeneratingName = (data) => {
+    // setSurnames(data.surnames);
+    console.log(data.surnames);
+    console.log(data.given_names);
+  };
+
+  useEffect(() => {
+    // console.log(data);
+    // console.log(surnames);
+    handleGeneratingName(data);
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center w-screen h-screen bg-cyan-700">
@@ -9,7 +27,12 @@ function App() {
             How many times would you like to generate(up to 10)
           </p>
           <div className="relative h-10 py-1.5 pl-3 pr-32 mb-5 rounded-xl bg-cyan-900">
-            <input type="number" className="w-full h-full bg-transparent" />
+            <input
+              type="number"
+              className="w-full h-full bg-transparent"
+              value={count}
+              onChange={(event) => setCount(event.target.value)}
+            />
             <button
               type="button"
               className="absolute top-0.5 bottom-0.5 right-0.5 box-border rounded-xl px-4 bg-cyan-950 uppercase"
@@ -18,10 +41,14 @@ function App() {
             </button>
           </div>
           <div className="w-full min-h-10 p-3 rounded-xl bg-cyan-900">
-            <p>
-              <span className="uppercase">Click on generate button</span> Rushil
-              Aggarwal
-            </p>
+            {isGenerated ? (
+              <p>name</p>
+            ) : (
+              <p>
+                <span className="uppercase">Click on generate button</span>{" "}
+                Rushil Aggarwal
+              </p>
+            )}
           </div>
         </div>
       </div>
